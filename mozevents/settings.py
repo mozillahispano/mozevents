d86c37cdc873e40c7cab82292ed25a613e4d173d
Nukeador <nukeador@gmail.com>
2013-03-17 19:49:14 +0100

    Changes for Django 1.5 (VI)

--- mozevents/settings.py
+++ mozevents/settings.py
@@ -82,6 +82,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
